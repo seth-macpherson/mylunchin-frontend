3fa6dadbdf6da2b48c310dcd003ecb3b2c68f7f8
seth-macpherson <seth.macpherson@gmail.com>
2011-10-10 14:18:34 -0700

    Added all the most basic of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylunchin::Application.config.secret_token = 'e64548fdf39fa749d256f67399c4572036ec87b25935cbe98edf102b39f147e22c1712a1aa6392b4e49f4200674cd0708e94a73b926b854d0a0fb6c838c6ab81'
